8283019f8d0938eaf884dd7599612d619efc857d
Takumi Ito <ito.tkmi@gmail.com>
2025-03-04 22:02:09 +0900

    Render list

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"log"
 	"os"
 
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"gopkg.in/yaml.v3"
 )
 
 type CommanderFile struct {
-	Commands    []Command
-	Environment map[string]string
+	Commands    []Command         `yaml:"commands"`
+	Environment map[string]string `yaml:"environment"`
 }
 
 type CommandInput struct {
@@ -40,25 +41,55 @@ func main() {
 	app := tview.NewApplication()
 	pages := tview.NewPages()
 
-	commandsPage := tview.NewBox().
-		SetBorder(true).
-		SetTitle(" Commander ")
+	commandsPage := tview.NewFlex().
+		SetDirection(tview.FlexRow)
+	pages.AddPage("Commands", commandsPage, true, true)
 
-	commandsPageWrapper := tview.NewFlex().
-		SetDirection(tview.FlexRow).
-		AddItem(commandsPage, 0, 1, true)
+	outputPage := tview.NewFlex().
+		SetDirection(tview.FlexRow)
+	pages.AddPage("Output", outputPage, true, false)
 
-	pages.AddPage("Commands", commandsPageWrapper, true, true)
+	commandsList := tview.NewList()
 
-	outputPage := tview.NewBox().
-		SetBorder(true).
-		SetTitle(" Output ")
+	for _, cmd := range commanderFile.Commands {
+		commandsList.AddItem(cmd.Title, "", 0, func() {
+			outputPage.Clear()
 
-	outputWrapper := tview.NewFlex().
-		SetDirection(tview.FlexRow).
-		AddItem(outputPage, 0, 1, true)
+			outputPageContent := tview.NewTextView().
+				SetWrap(true).
+				SetText(cmd.Template)
 
-	pages.AddPage("Output", outputWrapper, true, true)
+			outputPageContent.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+				if event.Key() == tcell.KeyRune && event.Rune() == 'q' {
+					pages.SwitchToPage("Commands")
+					return nil
+				}
+				return event
+			})
+
+			outputPage.AddItem(outputPageContent, 0, 1, true)
+
+			pages.SwitchToPage("Output")
+		})
+	}
+
+	commandsList.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyRune && event.Rune() == 'q' {
+			app.Stop()
+			return nil
+		}
+		return event
+	})
+
+	commandsPage.AddItem(commandsList, 0, 1, true)
+
+	commandsPage.
+		SetTitle(" Commands ").
+		SetBorder(true)
+
+	outputPage.
+		SetTitle(" Output ").
+		SetBorder(true)
 
 	if err := app.SetRoot(pages, true).SetFocus(pages).Run(); err != nil {
 		panic(err)
